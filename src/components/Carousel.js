06515402950f520d4ffec5d3c8e7270d6fbb5aaf
Komal-Bhurse <komalbhurse3@gmail.com>
2023-02-12 14:14:44 +0530

    bug solved

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -93,7 +93,7 @@ function Carousel() {
            {console.log(coins.map((e,index)=>{
             return e;
            }))}
-            {coins?.map((e, index) => {
+            {coins && coins.map((e, index) => {
               // calculating the profit 
               const profit = e.price_change_percentage_24h >= 0;
               const slide = (
